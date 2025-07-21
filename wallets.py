d137c110bf37f01a72eb854fe563b92ffda9b9f3
Tom Bini <bini.tom04@gmail.com>
2025-07-21 17:00:27 +0200

    Added donation wallet addresses

--- wallets.py
+++ wallets.py
@@ -2,14 +2,17 @@ wallets_evm = {
     "Main Wallet": "0x9c7AaA2876517920041769f0D385f8cBb8893086",
     "Ledger Main": "0x966c211bB663b6FAa4353814Fc85Ab88765732b1",
     "Farm auto test Wallet": "0xd5574ec4e804D63326b1EC9b26c34D014AC2ee6E",
-    "Wallet paumé 1" : "0x611A3938e8cEBeFa98B75124B14Ef7e1496d7e21"
+    "Wallet paumé 1": "0x611A3938e8cEBeFa98B75124B14Ef7e1496d7e21",
+    "Donation Wallet": "0xE0C8Ef01368a029ecbc4De2a93792AF3D4e883b6"
 }
 
 wallets_sui = {
-    "Ledger Main": "0xb9cd25f3777db149c61ce62a819dd4ad4935399095998a1e4b406d7d3979cb0a"
+    "Ledger Main": "0xb9cd25f3777db149c61ce62a819dd4ad4935399095998a1e4b406d7d3979cb0a",
+    "Donation Wallet": "0x77575b2c31680fd693d528cbfb0f3419be0b7e7341f948e5858f02c9a2e3e30a"
 }
 
 wallets_solana = {
     "Main Wallet": "DHmzvbLXE9HJWjS1P2SVAjTNV32sp4xWRMtbmn3TWFCi",
-    "Ledger Main": "9tfHcDsAPgZeAjWCwzC3aqY1C8NdfscDi5qAESt9vNNZ"
+    "Ledger Main": "9tfHcDsAPgZeAjWCwzC3aqY1C8NdfscDi5qAESt9vNNZ",
+    "Donation Wallet": "5F15nGw6eTJoTXSrRm8PoM2MDJswjNRgAeHgyCDanNnK"
 }
