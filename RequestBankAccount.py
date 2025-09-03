d02c494c4ac8675d15c92276f72fcca28b27a093
Tom-Bini <bini.tom04@gmail.com>
2025-09-03 20:38:46 +0200

    Updating Belfius Scraping

--- RequestBankAccount.py
+++ RequestBankAccount.py
@@ -9,6 +9,7 @@ from AssetSource import AssetSource
 from datetime import datetime
 from FetchExchangeRates import FetchExchangeRates
 
+
 class RequestBankAccount:
     def __init__(self, timestamp, rates: FetchExchangeRates):
         # Charger les clés depuis le fichier .env
@@ -19,64 +20,54 @@ class RequestBankAccount:
         self.REFRESH_TOKEN = os.getenv("GOCARDLESS_REFRESH_TOKEN")
         self.BANK_ID = os.getenv("GOCARDLESS_BANK_ID")
         self.REQUISITION_ID = os.getenv("GOCARDLESS_REQUISITION_ID")
-        
+
         self.btc_eur = rates.getBtcEur()
         self.btc_usd = rates.getBtcUsd()
         self.usd_eur = rates.getUsdEur()
-        
+
         self.timestamp = timestamp
 
     def get_tokens(self):
         # URL pour obtenir les tokens
         url = "https://bankaccountdata.gocardless.com/api/v2/token/new/"
-        
+
         # Prépare le payload avec secret_id et secret_key
-        payload = {
-            "secret_id": self.SECRET_ID,
-            "secret_key": self.SECRET_KEY
-        }
+        payload = {"secret_id": self.SECRET_ID, "secret_key": self.SECRET_KEY}
 
         # En-têtes HTTP requis
-        headers = {
-            'accept': 'application/json',
-            'Content-Type': 'application/json'
-        }
+        headers = {"accept": "application/json", "Content-Type": "application/json"}
 
         # Envoie la requête POST pour récupérer les tokens
         response = requests.post(url, headers=headers, json=payload)
-        
+
         # Vérifie si la requête a réussi
         if response.status_code != 200:
             print(f"Erreur {response.status_code} : {response.text}")
             response.raise_for_status()
-        
+
         # Si la réponse est OK, récupère les tokens
         data = response.json()
         new_access_token = data["access"]
         new_refresh_token = data["refresh"]
         set_key(".env", "GOCARDLESS_ACCESS_TOKEN", new_access_token)
         set_key(".env", "GOCARDLESS_REFRESH_TOKEN", new_refresh_token)
-        
 
     def refresh_token(self):
         url = "https://bankaccountdata.gocardless.com/api/v2/token/refresh/"
         headers = {
-            'accept': 'application/json',
-            'Content-Type': 'application/json',
+            "accept": "application/json",
+            "Content-Type": "application/json",
         }
-        data = {
-            "refresh": self.REFRESH_TOKEN
-        }
-        new_access_token = requests.post(url, headers=headers, json=data).json()["access"]
+        data = {"refresh": self.REFRESH_TOKEN}
+        new_access_token = requests.post(url, headers=headers, json=data).json()[
+            "access"
+        ]
         set_key(".env", "GOCARDLESS_ACCESS_TOKEN", new_access_token)
-
 
     # Étape 2 – Récupérer les comptes bancaires
     def get_accounts(self):
         url = "https://bankaccountdata.gocardless.com/api/v2/requisitions/8126e9fb-93c9-4228-937c-68f0383c2df7/"
-        headers = {
-            "Authorization": f"Bearer {self.ACCESS_TOKEN}"
-        }
+        headers = {"Authorization": f"Bearer {self.ACCESS_TOKEN}"}
         response = requests.get(url, headers=headers)
         response.raise_for_status()
         return response.json()
@@ -85,14 +76,14 @@ class RequestBankAccount:
         url = "https://bankaccountdata.gocardless.com/api/v2/requisitions/"
 
         data = {
-        "redirect": "http://www.yourwebpage.com",
-        "institution_id": self.BANK_ID
+            "redirect": "http://www.yourwebpage.com",
+            "institution_id": self.BANK_ID,
         }
 
         headers = {
-        "accept": "application/json",
-        "Content-Type": "application/json",
-        "Authorization": f"Bearer {self.ACCESS_TOKEN}"
+            "accept": "application/json",
+            "Content-Type": "application/json",
+            "Authorization": f"Bearer {self.ACCESS_TOKEN}",
         }
 
         response = requests.post(url, headers=headers, data=json.dumps(data))
@@ -107,28 +98,30 @@ class RequestBankAccount:
     def get_accounts(self):
         url = f"https://bankaccountdata.gocardless.com/api/v2/requisitions/{self.REQUISITION_ID}/"
         headers = {
-            'accept': 'application/json',
-            "Authorization": f"Bearer {self.ACCESS_TOKEN}"
+            "accept": "application/json",
+            "Authorization": f"Bearer {self.ACCESS_TOKEN}",
         }
-        response = requests.get(url, headers = headers).json()
+        response = requests.get(url, headers=headers).json()
         print(response)
         return response["accounts"]
 
     def get_account_details(self, account):
-        url = f"https://bankaccountdata.gocardless.com/api/v2/accounts/{account}/details/"
+        url = (
+            f"https://bankaccountdata.gocardless.com/api/v2/accounts/{account}/details/"
+        )
         headers = {
-            'accept': 'application/json',
-            "Authorization": f"Bearer {self.ACCESS_TOKEN}"
+            "accept": "application/json",
+            "Authorization": f"Bearer {self.ACCESS_TOKEN}",
         }
-        print(requests.get(url, headers = headers).json())
+        print(requests.get(url, headers=headers).json())
 
     def get_balances(self, accounts_list):
         balances_list = []
         for account in accounts_list:
             url = f"https://bankaccountdata.gocardless.com/api/v2/accounts/{account}/balances/"
             headers = {
-            'accept': 'application/json',
-            "Authorization": f"Bearer {self.ACCESS_TOKEN}"
+                "accept": "application/json",
+                "Authorization": f"Bearer {self.ACCESS_TOKEN}",
             }
             response = requests.get(url, headers=headers)
             if response.status_code == 200:
@@ -139,42 +132,59 @@ class RequestBankAccount:
 
         return balances_list
 
-    def get_bank_id(self, country = "be", bic = "GKCCBEBB"):
+    def get_bank_id(self, country="be", bic="GKCCBEBB"):
         url = f"https://bankaccountdata.gocardless.com/api/v2/institutions/?country={country}"
         headers = {
-            'accept': 'application/json',
-            "Authorization": f"Bearer {self.ACCESS_TOKEN}"
+            "accept": "application/json",
+            "Authorization": f"Bearer {self.ACCESS_TOKEN}",
         }
 
         bank_list = requests.get(url, headers=headers).json()
 
         for bank in bank_list:
-            if bank['bic'] == bic:
+            if bank["bic"] == bic:
                 print(f"Bank ID trouvé : {bank['id']}")
                 return bank["id"]
         print("Combinaison country + Bic non géré ou mauvais")
-        return 'false'
-    
+        return "false"
+
     def get_assets(self, amounts):
         assets_list = []
         for amount in amounts:
-            amount = float(amount.replace(',', ''))
-            assets_list.append(Asset("Euro", AssetType.FIAT, AssetSource.BELFIUS, self.timestamp, "EUR", self.usd_eur, self.btc_eur, self.btc_usd, amount, 1.0))
+            amount = float(amount.replace(",", ""))
+            assets_list.append(
+                Asset(
+                    "Euro",
+                    AssetType.FIAT,
+                    AssetSource.BELFIUS,
+                    self.timestamp,
+                    "EUR",
+                    self.usd_eur,
+                    self.btc_eur,
+                    self.btc_usd,
+                    amount,
+                    1.0,
+                )
+            )
         return assets_list
 
 
 if __name__ == "__main__":
+    from datetime import datetime
+    from FetchExchangeRates import FetchExchangeRates
+
+    timestamp = datetime.now()
+    rates = FetchExchangeRates()
 
     current_time = datetime.now()
-    rounded_time = current_time.replace(minute = 0, second = 0, microsecond = 0)
+    rounded_time = current_time.replace(minute=0, second=0, microsecond=0)
     timestamp = rounded_time.strftime("%Y-%m-%d %H")
 
-    hourlyRequestBank = RequestBankAccount(timestamp)
-    
-    accounts_list = hourlyRequestBank.get_accounts()
-    print(accounts_list)
-    balances = hourlyRequestBank.get_balances(accounts_list)
-    print(balances)
-    assets = hourlyRequestBank.get_assets(balances)
-    print(assets)
-    
+    hourlyRequestBank = RequestBankAccount(timestamp, rates)
+    hourlyRequestBank.create_link()
+    # accounts_list = hourlyRequestBank.get_accounts()
+    # print(accounts_list)
+    # balances = hourlyRequestBank.get_balances(accounts_list)
+    # print(balances)
+    # assets = hourlyRequestBank.get_assets(balances)
+    # print(assets)
